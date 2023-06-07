92bf1634c157fd79330acafbe7ea390d58216df5
Francisco Rodriguez <fjrodriguez.353@icloud.com>
2023-06-07 22:21:34 +0200

    feat: fix cards

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Card as NextCard, Button } from '@nextui-org/react'
 import { FC } from 'react'
 import { Wahstapp } from '../icons/whatsapp'
@@ -18,52 +19,36 @@ export const Card: FC<FCCard> = ({
   })
 
   return (
-    <NextCard className={styles.card}>
-      <NextCard.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
-        <p className={styles.tag}>{category}</p>
-      </NextCard.Header>
-      <NextCard.Body css={{ p: 0 }}>
-        <NextCard.Image
-          src={image}
-          width='100%'
-          height='100%'
-          objectFit='cover'
-          alt={title}
-        />
-      </NextCard.Body>
-      <NextCard.Footer
-        isBlurred
-        css={{
-          bgBlur: '#ffffff66',
-          borderTop: '$borderWeights$light solid rgba(255, 255, 255, 0.2)',
+    <article className={styles.card}>
+      <p className={styles.category}>{category}</p>
+      <div
+        className={styles.photo}
+        style={{
+          backgroundImage: `url(${image})`,
         }}
-        className={styles.footer}
-      >
-        <div className={styles.info}>
-          <span className={styles.top}>
-            <h4>{title}</h4>
-            <p>{currencyFormatter(price)}</p>
-          </span>
-          <p>{description}</p>
-        </div>
-        <span className={styles.actions}>
-          <Button
-            auto
-            className={styles.whatsapp}
-            color='success'
-            flat
-            icon={<Wahstapp />}
-            rounded
-          >
-            <a
-              href={`https://wa.me/5492657392766?text=Hola,%20como%20estas%20Agustina?%20Estoy%20interesado%20en%20"${title}"`}
-              target='_blank'
-            >
-              Me interesa
-            </a>
-          </Button>
+      />
+      <div className={styles.info}>
+        <span className={styles.top}>
+          <h4>{title}</h4>
+          <p>{currencyFormatter(price)}</p>
         </span>
-      </NextCard.Footer>
-    </NextCard>
+        <p>{description}</p>
+      </div>
+      <Button
+        auto
+        className={styles.whatsapp}
+        color='success'
+        flat
+        icon={<Wahstapp />}
+        rounded
+      >
+        <a
+          href={`https://wa.me/5492657392766?text=Hola,%20como%20estas%20Agustina?%20Estoy%20interesado%20en%20"${title}"`}
+          target='_blank'
+        >
+          Me interesa
+        </a>
+      </Button>
+    </article>
   )
 }
